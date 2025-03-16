e195598953940f8d1269712f4f6f4f765809dc43
cnabwire <c.omondi@alustudent.com>
2025-03-16 03:19:44 +0000

    mend

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,39 +1,61 @@
 #!/usr/bin/python3
-"""Script that gets user data (Todo list) from API
-and then export the result to a CSV file."""
+"""Script to export user TODO list data to a CSV file."""
 
 import csv
 import requests
 import sys
 
-def main():
-    """Main function"""
-    user_id = int(sys.argv[1])
-    todo_url = 'https://jsonplaceholder.typicode.com/todos'
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
 
-    # Get the employee data (username)
+def fetch_user_data(user_id):
+    """Fetches user information from the API."""
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     response = requests.get(user_url)
-    user_name = response.json().get('username')
+    if response.status_code != 200:
+        print("Error: Unable to fetch user data")
+        sys.exit(1)
+    return response.json()
 
-    # Get the todos data
+
+def fetch_todo_data(user_id):
+    """Fetches TODO list data from the API."""
+    todo_url = (
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
+    )
     response = requests.get(todo_url)
-    tasks = response.json()
+    if response.status_code != 200:
+        print("Error: Unable to fetch TODO data")
+        sys.exit(1)
+    return response.json()
 
-    # File name
+
+def export_to_csv(user_id, username, tasks):
+    """Exports tasks to a CSV file in the required format."""
     file_name = "{}.csv".format(user_id)
-
-    # Open the CSV file and write the data
     with open(file_name, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-
-        # Write the header row
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-        # Write each task data for the specific user
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in tasks:
-            if task.get('userId') == user_id:
-                writer.writerow([user_id, user_name, task.get('completed'), task.get('title')])
+            writer.writerow(
+                [user_id, username, task['completed'], task['title']]
+            )
+
+
+def main():
+    """Main function."""
+    if len(sys.argv) != 2:
+        print("Usage: python3 1-export_to_CSV.py <USER_ID>")
+        sys.exit(1)
+
+    try:
+        user_id = int(sys.argv[1])
+    except ValueError:
+        print("Error: USER_ID must be an integer")
+        sys.exit(1)
+
+    user_data = fetch_user_data(user_id)
+    todo_data = fetch_todo_data(user_id)
+    export_to_csv(user_id, user_data['username'], todo_data)
+    print("Data successfully exported to {}.csv".format(user_id))
+
 
 if __name__ == "__main__":
     main()
