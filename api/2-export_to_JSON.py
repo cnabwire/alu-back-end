2cac1f6fd3b2a3fec0d113d8b2c44b7c3ad4e9b6
cnabwire <c.omondi@alustudent.com>
2025-03-22 03:52:25 +0000

    Dictionary list of dictionaries

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,62 +1,36 @@
 #!/usr/bin/python3
-"""Script to export user TODO list data to a JSON file."""
+"""script that gets user data (Todo list) from API
+and then export the result to csv file. """
 
 import json
 import requests
 import sys
 
 
-def fetch_user_data(user_id):
-    """Fetches user information from the API."""
-    user_url = "https://jsonplaceholder.typicode.com/users/" + str(user_id)
-    response = requests.get(user_url)
-    if response.status_code != 200:
-        print("Error: Unable to fetch user data")
-        sys.exit(1)
-    return response.json()
-
-
-def fetch_todo_data(user_id):
-    """Fetches TODO list data from the API."""
-    todo_url = "https://jsonplaceholder.typicode.com/todos?userId=" + str(user_id)
-    response = requests.get(todo_url)
-    if response.status_code != 200:
-        print("Error: Unable to fetch TODO data")
-        sys.exit(1)
-    return response.json()
-
-
-def export_to_json(user_id, username, tasks):
-    """Exports tasks to a JSON file in the required format."""
-    file_name = str(user_id) + ".json"
-    task_list = []
-    for task in tasks:
-        task_list.append({
-            "task": task["title"],
-            "completed": task["completed"],
-            "username": username
-        })
-    data = {str(user_id): task_list}
-    with open(file_name, mode="w", encoding="utf-8") as file:
-        json.dump(data, file)
-
-
 def main():
-    """Main function."""
-    if len(sys.argv) != 2:
-        print("Usage: python3 2-export_to_JSON.py <USER_ID>")
-        sys.exit(1)
-    try:
-        user_id = int(sys.argv[1])
-    except ValueError:
-        print("Error: USER_ID must be an integer")
-        sys.exit(1)
-    user_data = fetch_user_data(user_id)
-    todo_data = fetch_todo_data(user_id)
-    export_to_json(user_id, user_data["username"], todo_data)
-    print("Data successfully exported to", str(user_id) + ".json")
+    """main function"""
+    user_id = int(sys.argv[1])
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+
+    response = requests.get(todo_url)
+    user_name = requests.get(user_url).json().get('username')
+    user_data = []
+    output = {user_id: user_data}
+
+    for todo in response.json():
+        if todo.get('userId') == user_id:
+            user_data.append(
+                {
+                    "task": todo.get('title'),
+                    "completed": todo.get('completed'),
+                    "username": user_name,
+                })
+    print(output)
+    file_name = "{}.json".format(user_id)
+    with open(file_name, 'w') as file:
+        json.dump(output, file)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
